c82396aa7eaf271edc5c4594223fb8d7ced44b1c
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2023-07-21 17:25:12 +0100

    uhhh - added website thing - more images - added recent.json for the website

--- src/run-place.ts
+++ src/run-place.ts
@@ -1,12 +1,21 @@
 import puppeteer from "puppeteer";
 import { run } from "./place";
 import config from "./config";
+import { writeFileSync } from "fs";
 
 (async () => {
 	const browser = await puppeteer.launch(config.puppeteer);
-
-	await run(browser)
+	function callback(filename: string) {
+		writeFileSync(
+			"recent.json",
+			JSON.stringify({
+				date: Number(filename.split("-")[1].split(".")[0]),
+				filename,
+			}),
+		);
+	}
+	await run(browser, (_, a) => callback(a));
 	setInterval(async () => {
-		await run(browser);
+		await run(browser, (_, a) => callback(a));
 	}, config.interval);
-})()
+})();
