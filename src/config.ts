adbd7a8d44bc5d48a4d8746b81f119e181dee470
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2023-07-21 20:37:47 +0100

    EVERY MINUTE HAHAHA

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export default {
 		headless: false,
 		executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
 	},
-	interval: 1000 * 60 * 3,
+	interval: 1000 * 60,
 } satisfies {
 	puppeteer?: PuppeteerLaunchOptions;
 	/** in ms, not used for `once` script */
